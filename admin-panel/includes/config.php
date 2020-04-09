58ed26d348a2688257a301f3ccbdc5fff35740ad
Prabhat Sharma <prabhat.04011998@gmail.com>
2020-04-09 13:51:27 +0530

    image add

--- admin-panel/includes/config.php
+++ admin-panel/includes/config.php
@@ -8,7 +8,7 @@ $USERNAME = 'phpmyadmin';
 $PASSWORD = 'prabhat';
 $DBPORT='3306';
 
-$apiKey="141dd10d53b3ebf184a7802a8ca42b75";
+$apiKey="apikey here";
 
 $db=mysqli_connect($localhost,$USERNAME,$PASSWORD,$DBNAME);
 if($db === false){
